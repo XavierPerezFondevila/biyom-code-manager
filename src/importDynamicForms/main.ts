3908001bfc8863cb30a6e238963ba4fe1720448d
Joelthorner <joeltorner@gmail.com>
2023-07-13 16:08:23 +0200

    Add importDynamicForms confirmation

--- src/importDynamicForms/main.ts
+++ src/importDynamicForms/main.ts
@@ -1,33 +1,41 @@
 import * as fs from "fs";
+import * as vscode from "vscode";
 import { repoPath } from "../extension";
 import { controllers } from "./files/controllers";
 import { enums } from "./files/enums";
 import * as utils from './utils/utils';
 
-export function init(){
+export async function init() {
   if (!repoPath.length) {
     return;
   }
 
-  console.log(isModuleInstalled());
+  if (isModuleInstalled()) {
+    return;
+  }
 
-  if(isModuleInstalled()){
+  const result = await vscode.window.showQuickPick(['Yes', 'No'], {
+    placeHolder: 'Are you sure you want to import the Dynamic Forms lib?',
+    canPickMany: false,
+  });
+
+  if (result !== 'Yes') {
     return;
   }
 
   createFiles();
 }
 
-const isModuleInstalled = ():boolean => {
+const isModuleInstalled = (): boolean => {
   const filePath = repoPath + '\\src\\Core\\Theme\\Dtos\\Commerce.php';
   const fileContent = fs.readFileSync(filePath, "utf8").split(/\r?\n/);
-  
+
   return fileContent.findIndex((line) => line.includes('DynamicCustomForms')) !== -1;
 };
 
 const createFiles = () => {
-  const internalFiles = controllers.concat(enums); 
-  
+  const internalFiles = controllers.concat(enums);
+
   utils.addCommerceFunction();
   internalFiles.forEach((file: any) => {
     utils.customWriteFile(file.path, file.name, file.content);
